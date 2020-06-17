5a70263089140f38ec2227c350572d499014b9ea
tjdanqkr <tjdanqkr@gmail.com>
2020-06-18 02:32:12 +0900

    완성

--- restapi/__init__.py
+++ restapi/__init__.py
@@ -39,13 +39,14 @@ def instar():
     word = json.get('word')
     print(word)
     insta = []
-    my_db = client['Project']
+    my_db = client['Insta']
     mycol = my_db[word]
     num = json.get('num')
     my_doc1 = mycol.find()
     i=0
     for i in range(int(num)):
-        q={data:json.get('date'+str(i))}
+        #q={data:json.get('date'+str(i))}
+        q=dict(date=json.get('date'+str(i)))
         print("a")
         print(q)
         my_doc = mycol.find(q,{"_id":0,"tags":1})
@@ -98,6 +99,82 @@ def yearchui():
     print(my_doc)
     client.close()
     return jsonify(my_doc)
+
+@app.route("/chuigr",methods=['POST'])
+def chuigr():
+    my_db = client['Project']
+    mycol = my_db['SalesTrend']
+    dong = request.get_json()
+
+    my_doc = list(mycol.find({"동명":dong.get("dong")},{"_id":0,"기준_년_코드":1,"기준_분기_코드":1,"분기별_매출":1,"상권_코드_명":1}))
+
+    for i in range(len(my_doc)):
+                    print(i)
+
+    print(my_doc)
+    client.close()
+    return jsonify(my_doc)
+
+@app.route("/instar2",methods=['GET'])
+def instar2():
+
+    my_db = client['Insta']
+    mycol = my_db['insta서초구맛집']
+    my_doc = mycol.distinct('date')
+    print(my_doc)
+
+    a={}
+    a={"date":my_doc}
+
+    print(a)
+    client.close()
+    return jsonify(a)
+
+@app.route("/gilcucheon",methods=['POST'])
+def gilcucheon():
+    my_db = client['Project']
+    mycol = my_db['upjong']
+    value = request.get_json()
+    my_doc = list(mycol.find({"행정동명":value.get('dong')},{"_id":0}))
+    print(my_doc)
+
+    c=[]
+    for i in range(len(my_doc)):
+        gil=my_doc[i]['상권_코드_명']
+        avg = (float(my_doc[i]["분식전문점"])+float(my_doc[i]["안경"])+float(my_doc[i]["일식음식점"])+float(my_doc[i]["제과점"])
+        +float(my_doc[i]["패스트푸드점"])+float(my_doc[i]["편의점"])+float(my_doc[i]["한식음식점"])+float(my_doc[i]["호프-간이주점"])+float(my_doc[i]["화장품"]))/9
+        coffeeavg = avg /(float(my_doc[i]["커피-음료"]))
+
+        b={"gil":gil,"avg평균매출":avg,"coffee평균매출":coffeeavg}
+        c.append(b)
+
+
+
+    for i in range(len(c)):
+        for j in range(len(c)-1):
+            if(float(c[i]['coffee평균매출']) > float(c[j]['coffee평균매출'])):
+                    print(i, j)
+                    print(float(c[i]['coffee평균매출']),float(c[j]['coffee평균매출']))
+                    c[i]["gil"],c[j]["gil"]  =c[j]["gil"],c[i]["gil"]
+                    c[i]["avg평균매출"],c[j]["avg평균매출"]=c[j]["avg평균매출"],c[i]["avg평균매출"]
+                    c[i]["coffee평균매출"],c[j]["coffee평균매출"] = c[j]["coffee평균매출"],c[i]["coffee평균매출"]
+
+            elif (float(c[i]['coffee평균매출']) >= float(c[j]['coffee평균매출'])):
+                continue
+
+    a=[]
+    print(len(c))
+    if(len(c)>=5):
+        for i in range(5):
+            a.append(c[i])
+    else:
+        for i in range(len(c)):
+            a.append(c[i])
+    print(a)
+    client.close()#mongoDB닫는것.
+
+    return jsonify(a)
+
 
 @app.route("/server_info")
 def server_json():
